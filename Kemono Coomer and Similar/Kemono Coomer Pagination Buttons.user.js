b255938de963d86a86bdaf4fb93e2a715ba55f8e
scriptfor <Sikiri@protonmail.com>
2025-04-28 09:57:40 +0100

    Update Kemono Coomer Pagination Buttons.user.js

--- Kemono Coomer and Similar/Kemono Coomer Pagination Buttons.user.js
+++ Kemono Coomer and Similar/Kemono Coomer Pagination Buttons.user.js
@@ -3,9 +3,9 @@
 // @namespace    http://tampermonkey.net
 // @version      2025-04-24_02-06
 // @description  Adds pagination buttons '<' (on the left) and '>' (on the right) to the page, allowing for quick page changes. It also serves as an alternative to a bug that causes pagination to disappear due to blacklisting. This script is compatible with the sites kemono, coomer, and nekohouse.
-// @match        *://*kemono.su/posts*
-// @match        *://*coomer.su/posts*
-// @match        *://*nekohouse.su/*
+// @match        *://kemono.su/posts*
+// @match        *://coomer.su/posts*
+// @match        *://nekohouse.su/*
 // @author       scriptfor
 // @homepageURL  https://github.com/scriptfor
 // @updateURL    https://github.com/scriptfor/-/raw/refs/heads/'/Kemono%20Coomer%20and%20Similar/Kemono%20Coomer%20Pagination%20Buttons.meta.js
